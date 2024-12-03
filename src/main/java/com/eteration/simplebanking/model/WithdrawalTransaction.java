6518d21a4193ff450e76d68a47dc2f89cf13721d
servettartar <servet_tartar@hotmail.com>
2024-12-03 23:24:45 +0300

    build

--- src/main/java/com/eteration/simplebanking/model/WithdrawalTransaction.java
+++ src/main/java/com/eteration/simplebanking/model/WithdrawalTransaction.java
@@ -1,13 +1,16 @@
 package com.eteration.simplebanking.model;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-@Data
+@Getter
+@Setter
 @Entity
 @DiscriminatorValue("WITHDRAWAL")
 @NoArgsConstructor
